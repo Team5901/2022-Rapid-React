f926a4e4c0323c571992f269c980d93af8054c65
Team5901 <35078583+Team5901@users.noreply.github.com>
2022-02-22 19:11:56 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -44,7 +45,12 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
     
     //auto = new SendableChooser<>();
-    CameraServer.startAutomaticCapture();
+    
+    // Get the UsbCamera from CameraServer
+    UsbCamera camera = CameraServer.startAutomaticCapture();
+    // Set the resolution
+    camera.setResolution(320, 240);
+    
 
     SendableChooser<String> auto = new SendableChooser<String>();
 

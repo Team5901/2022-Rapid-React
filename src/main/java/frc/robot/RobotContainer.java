944972443afd7cd7f8c58aa1298fa89ffb9a7e1f
will <21347807+datalal@users.noreply.github.com>
2022-03-01 18:19:34 -0500

    Add ShootHigh To Autonomous Command Sequence

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -132,7 +132,8 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    System.out.println("Hi");
-    return new AutoDrive(200.0,m_DrivetrainSubsystem);  
+    System.out.println("RUNNING AUTONOMOUS COMMANDS");
+    return new AutoDrive(200.0,m_DrivetrainSubsystem)  
+      .andThen(new ShootHigh(ShooterSubsystem));
   }
 }
